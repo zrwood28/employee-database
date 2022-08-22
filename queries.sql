4f42303bbf40e9d14360551fa8f64553c24201ac
Zach Wood <zachrwood28@gmail.com>
2022-08-22 18:02:12 -0500

    Added more queries.

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-select e.emp_no, e.first_name, e.last_name, e.sex, s.salary
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 from employees as e
 join salaries as s
 on e.emp_no = s.emp_no;
@@ -7,13 +7,24 @@ select first_name, last_name, hire_date
 from employees
 where hire_date between '1986-01-01' and '1986-12-31';
 
-select * from dept_manager;
-
-select * from departments;
-
-select * from employees;
-
-select dm.dept_no, e.emp_no, e.last_name, e.first_name
+select dm.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
 from employees as e
 join dept_manager as dm
-on e.emp_no = dm.emp_no;
+on e.emp_no = dm.emp_no
+	join departments as d
+	on d.dept_no = dm.dept_no;
+		
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees as e
+join dept_emp as de
+on e.emp_no = de.emp_no
+	join departments as d
+	on de.dept_no = d.dept_no;
+	
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules'
+	and last_name like 'B%';
+	
+
+	
